f6827229fb090ccd3430731043df11d129e5b990
Revanth Kumar <revanth.kumar@colortokens.com>
2021-02-20 19:06:17 +0530

    intro sorting

---
+++ problem solving/algorithms/sorting/intro-sorting.java
@@ -0,0 +1,61 @@
+/**
+https://www.hackerrank.com/challenges/tutorial-intro/problem
+ */
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the introTutorial function below.
+    static int introTutorial(int V, int[] arr) {
+        int first=0,last=arr.length-1,middle;
+        while(true)
+        {
+            middle=(first+last)/2;
+            if(arr[middle]==V)return middle;
+            else if(arr[middle]>V)
+            {
+                last=middle-1;
+            }
+            else first=middle+1;
+        }
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int V = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = introTutorial(V, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
