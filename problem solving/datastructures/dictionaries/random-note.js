9461ce497d80d8e73065c3a132ff94fe49a0588b
Revanth Kumar <revanthmadasu913@gmail.com>
2021-03-11 23:50:14 +0530

    is dictionary subset or not

---
+++ problem solving/datastructures/dictionaries/random-note.js
@@ -0,0 +1,50 @@
+'use strict';
+// https://www.hackerrank.com/challenges/ctci-ransom-note/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function checkMagazine(magazine, note) {
+    const magazineMap = {};
+    magazine.forEach(word => magazineMap[word]=(magazineMap[word] || 0)+1);
+    console.log((note.every(word => {
+        if (magazineMap[word]) {
+            --magazineMap[word];
+        } else {
+            return false;
+        }
+        return true;        
+        }) ? 'Yes' : 'No'));
+}
+
+function main() {
+    const mn = readLine().split(' ');
+
+    const m = parseInt(mn[0], 10);
+
+    const n = parseInt(mn[1], 10);
+
+    const magazine = readLine().split(' ');
+
+    const note = readLine().split(' ');
+
+    checkMagazine(magazine, note);
+}
