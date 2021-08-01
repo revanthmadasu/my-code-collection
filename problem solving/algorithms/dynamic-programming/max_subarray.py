4d10e73def62cccd030d7527580359900c0073a1
Revanth Kumar <revanthmadasu913@gmail.com>
2021-08-01 21:23:13 +0530

    max sub array - dynamic programming

---
+++ problem solving/algorithms/dynamic-programming/max_subarray.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/maxsubarray/problem
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxSubarray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def maxSubarray(arr):
+    maxSubSequence = float('-inf')
+    maxSubArray = 0
+    cur_sum = float('-inf')
+    max_sum = float('-inf')
+    for num in arr:
+        maxSubSequence = max((maxSubSequence+num), num, maxSubSequence)
+        cur_sum = num if cur_sum+num < num else cur_sum+num
+        if max_sum < cur_sum:
+            max_sum = cur_sum
+    return (max_sum,maxSubSequence)
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = maxSubarray(arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
