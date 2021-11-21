4a2618ebd000f1387eb2658a392aff2f833d8283
Revanth Madasu <revanthmadasu913@gmail.com>
2021-11-21 21:12:56 +0530

    binary search

---
+++ problem solving/algorithms/searching/binary-search.py
@@ -0,0 +1,13 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        mid = int(n/2)
+        if target == nums[mid]:
+            return mid
+        if n == 1:
+            return -1
+        if target < nums[mid]:
+            return self.search(nums[0:mid], target)
+        else:
+            res = self.search(nums[mid:], target)
+            return -1 if res == -1 else mid + res
