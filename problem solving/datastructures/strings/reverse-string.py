1db9594edf9a442a1af073f722b90920929f8fc1
Revanth Madasu <revanthmadasu913@gmail.com>
2021-12-04 12:32:10 +0530

    removed type

--- problem solving/datastructures/strings/reverse-string.py
+++ problem solving/datastructures/strings/reverse-string.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/reverse-string/
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s):
         """
         Do not return anything, modify s in-place instead.
         """
