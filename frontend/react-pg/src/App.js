4afedb052b651991570ed4adf89749b6bd185223
Revanth Madasu <revanthmadasu913@gmail.com>
2023-05-24 20:58:21 -0500

    auto complete component

--- frontend/react-pg/src/App.js
+++ frontend/react-pg/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import AutoComplete from './components/auto-complete';
+import { useCallback, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+    const refreshSuggestions = useCallback((text) => {
+        const lowerText = text.toLowerCase();
+        const newSuggestions = allSuggestions.filter(suggestion => suggestion.indexOf(lowerText) !== -1);
+        console.log(`new suggestions are ${newSuggestions}`);
+        setSuggestions(newSuggestions);
+    }, []);
+    const allSuggestions = ["hello", "how are you", "bye", "good morning", "good evening", "good night", "whatsapp", "ssupp"];
+    const [suggestions, setSuggestions] = useState(allSuggestions);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <AutoComplete onTextChange={refreshSuggestions} suggestions={suggestions}/>
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/react-pg/src/components/auto-complete/AutoComplete.jsx
@@ -0,0 +1,22 @@
+import { useCallback, useState, useMemo } from "react"
+import "./AutoComplete.css";
+export const AutoComplete = ({onTextChange, suggestions}) => {
+    const [text, setText] = useState('');
+    const onTextInput = useCallback((event) => {
+        console.log(event.target.value);
+        onTextChange(event.target.value);
+    }, [onTextChange]);
+    const suggestionsTags = useMemo(() => {
+        return suggestions.map(suggestion => <li className="list-group-item">{suggestion}</li>);
+    }, [suggestions]);
+    return <>    
+        <div className="auto-complete position-relative d-inline-flex">
+            <input type="text" onKeyDown={onTextInput} />
+            <div className="suggestions d-inline-block position-absolute">
+                <ul className="list-group suggestions-list">
+                    {suggestionsTags}
+                </ul>
+            </div>
+        </div>
+    </>
+};
