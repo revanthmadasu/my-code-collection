d69a20469447567bda7693247d6a57de791dc0fb
sindhujakandadi <sindhujakandadi@gmail.com>
2023-06-07 16:57:05 -0500

    article view

---
+++ sql/basic-selects/ArticleViews.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/article-views-i
+-- concepts: distinct, order by, select
+
+select distinct author_id as id
+from Views
+where author_id = viewer_id
+order by author_id;
