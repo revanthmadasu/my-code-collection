016970b428f87fbd0702d75c0891b21faec1c088
Revanth Madasu <revanthmadasu913@gmail.com>
2023-07-23 17:59:38 -0500

    remove element - 2 pointer approach

--- problem solving/datastructures/arrays/remove_element.py
+++ problem solving/datastructures/arrays/remove_element.py
@@ -1,15 +1,37 @@
 '''
     problem: https://leetcode.com/problems/remove-element
-    concepts: arrays
-    runtime: 17.6% memory: 37.8%
+    concepts: arrays, two pointers
+    runtime: 84.33% memory: 73.8%
 '''
 class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        res_nums = []
-        for num in nums:
-            if num != val:
-                res_nums.append(num)
-        k = len(res_nums)
-        for i in range(k):
-            nums[i] = res_nums[i]
-        return k        
+    # low performance: runtime: 17.6% memory: 37.8%
+    # def removeElement(self, nums: List[int], val: int) -> int:
+    #     res_nums = []
+    #     for num in nums:
+    #         if num != val:
+    #             res_nums.append(num)
+    #     k = len(res_nums)
+    #     for i in range(k):
+    #         nums[i] = res_nums[i]
+    #     return k  
+
+    # 
+    def removeElement(self, nums: List[int], val: int) -> int:   
+        n = len(nums)    
+        p1 , p2 = (0, 0)
+        k = 0
+        if not n:
+            return 0
+        while p2 < n:
+            # print(f'initial p1: {p1} p2: {p2}')
+            while p2 < n and nums[p2] == val:
+                # print(f'skipping {p2}')
+                p2 += 1
+                k+=1 
+            # print(f'after skipping p1: {p1} p2: {p2}')
+            if p2 >= n:
+                break
+            nums[p1] = nums[p2]
+            p1+=1
+            p2+=1
+        return n-k      
