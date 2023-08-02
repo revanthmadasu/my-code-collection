40b267d1d8a952d274382055ed5c476ef5a321cb
Revanth Madasu <revanthmadasu913@gmail.com>
2023-08-02 07:21:31 -0500

    added all fruits

--- frontend/sau/indian-fruits/javascript/description.js
+++ frontend/sau/indian-fruits/javascript/description.js
@@ -141,6 +141,270 @@ const fruits = [
     }
 ];
 
+const fruits2 = [
+    {
+        "id": "raspberry",
+        "name": "Raspberry",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/raspberry.jpeg",
+                "description": "Raspberry is a small, red or black edible fruit that grows on raspberry plants. Known for its vibrant color and sweet-tart flavor, raspberries are often enjoyed fresh, added to desserts, or used to make jams and preserves."
+            }
+        ]
+    },
+    {
+        "id": "blueberry",
+        "name": "Blueberry",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/blueberry.jpeg",
+                "description": "Blueberry is a small, round, and sweet fruit that comes in shades of blue and purple. Packed with antioxidants and vitamins, blueberries are popularly consumed fresh, added to cereals, baked into muffins, or blended into smoothies."
+            }
+        ]
+    },
+    {
+        "id": "watermelon",
+        "name": "Watermelon",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/watermelon.jpeg",
+                "description": "Watermelon is a large, juicy fruit with a sweet and refreshing taste. It has a high water content and is often enjoyed in slices during hot weather or used in fruit salads and beverages."
+            }
+        ]
+    },
+    {
+        "id": "muskmelon",
+        "name": "Musk Melon",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/muskmelon.jpeg",
+                "description": "Musk Melon, also known as Cantaloupe, is a sweet and aromatic fruit with a netted rind. It's rich in vitamins and minerals, and its flesh can range from pale orange to deep coral in color."
+            }
+        ]
+    },
+    {
+        "id": "cantaloupe",
+        "name": "Cantaloupe",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/cantaloupe.jpeg",
+                "description": "Cantaloupe, also known as Muskmelon, is a sweet and flavorful fruit with a distinctive orange flesh and a netted rind. It's commonly enjoyed fresh, in fruit salads, or as a refreshing snack."
+            }
+        ]
+    },
+    {
+        "id": "apple",
+        "name": "Apple",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/apple.jpeg",
+                "description": "Apple is a widely cultivated fruit with a variety of colors and flavors. It is known for its crisp texture and is often enjoyed raw, cooked in dishes, or used to make juices and cider."
+            }
+        ]
+    },
+    {
+        "id": "pear",
+        "name": "Pear",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/pear.jpeg",
+                "description": "Pear is a sweet and juicy fruit with a distinctive bell shape. It comes in various colors and varieties, and it can be enjoyed fresh, added to salads, or used in baking and cooking."
+            }
+        ]
+    },
+    {
+        "id": "kiwi",
+        "name": "Kiwi",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/kiwi.jpeg",
+                "description": "Kiwi, also known as kiwifruit, is a small, fuzzy fruit with vibrant green flesh and tiny black seeds. It has a tangy and sweet flavor, and it's often eaten as a fresh fruit or added to salads and desserts."
+            }
+        ]
+    },
+    {
+        "id": "dragon-fruit",
+        "name": "Dragon Fruit (Pitaya)",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/dragon-fruit.jpeg",
+                "description": "Dragon Fruit, also known as Pitaya, is a tropical fruit with a vibrant pink or yellow skin and white or red flesh dotted with black seeds. It has a mild, sweet flavor and is commonly eaten on its own or added to smoothies and bowls."
+            }
+        ]
+    },
+    {
+        "id": "avocado",
+        "name": "Avocado",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/avocado.jpeg",
+                "description": "Avocado is a creamy fruit with a rich texture and a mild, nutty flavor. It is often enjoyed sliced on toast, added to salads, or used to make guacamole."
+            }
+        ]
+    },
+    {
+        "id": "jamun",
+        "name": "Jamun (Java Plum)",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/jamun.jpeg",
+                "description": "Jamun, also known as Java Plum, is a small, dark purple fruit with a tangy and slightly sweet taste. It is commonly consumed fresh, used to make jams and preserves, or incorporated into traditional dishes."
+            }
+        ]
+    },
+    {
+        "id": "amla",
+        "name": "Amla (Indian Gooseberry)",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/amla.jpeg",
+                "description": "Amla, also known as Indian Gooseberry, is a green fruit with a sour and tangy flavor. It is highly valued in Ayurvedic medicine and is consumed fresh, dried, or used in various culinary preparations."
+            }
+        ]
+    },
+    {
+        "id": "thompson-seedless",
+        "name": "Thompson Seedless",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/thompson-seedless.jpeg",
+                "description": "Thompson Seedless grapes are a popular variety of green grapes known for their sweet and juicy taste. They are often eaten as a snack, used in salads, or enjoyed as a dessert."
+            }
+        ]
+    },
+    {
+        "id": "black-grapes",
+        "name": "Black Grapes",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/black-grapes.jpeg",
+                "description": "Black grapes are a variety of grapes with a deep purple to black color. They are sweet and flavorful, and they can be eaten fresh, used in wines, or added to a variety of dishes."
+            }
+        ]
+    },
+    {
+        "id": "green-grapes",
+        "name": "Green Grapes",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/green-grapes.jpeg",
+                "description": "Green grapes, also known as white grapes, are sweet and crisp. They are enjoyed as a refreshing snack, added to fruit platters, or used in making grape-based products like juices and jams."
+            }
+        ]
+    },
+    {
+        "id": "coconut",
+        "name": "Coconut",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/coconut.jpeg",
+                "description": "Coconut is a tropical fruit known for its hard brown shell and white, edible flesh. It has a distinct flavor and is used in a wide range of culinary dishes, desserts, and beverages."
+            }
+        ]
+    },
+    {
+        "id": "date-palm",
+        "name": "Date Palm",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/date-palm.jpeg",
+                "description": "Date Palm produces dates, which are sweet and chewy fruits. They are commonly consumed as a natural sweetener, used in baking, or enjoyed as a snack."
+            }
+        ]
+    },
+    {
+        "id": "fig",
+        "name": "Fig",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/fig.jpeg",
+                "description": "Fig is a unique fruit with a soft, pear-shaped exterior and sweet, chewy flesh filled with tiny seeds. Figs are enjoyed fresh, dried, or used in both sweet and savory dishes."
+            }
+        ]
+    },
+    {
+        "id": "black-mulberry",
+        "name": "Black Mulberry",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/black-mulberry.jpeg",
+                "description": "Black Mulberry is a dark-purple fruit that grows on mulberry trees. It has a sweet and juicy flavor and can be eaten fresh, used in jams, or added to desserts."
+            }
+        ]
+    },
+    {
+        "id": "white-mulberry",
+        "name": "White Mulberry",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/white-mulberry.jpeg",
+                "description": "White Mulberry is a sweet and flavorful fruit that comes from the mulberry tree. It can be enjoyed fresh, dried, or used in various culinary applications."
+            }
+        ]
+    },
+    {
+        "id": "kinnow",
+        "name": "Kinnow",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/kinnow.jpeg",
+                "description": "Kinnow is a hybrid citrus fruit with a tangy and sweet taste. It is similar to oranges and is often consumed fresh or used to make juices."
+            }
+        ]
+    },
+    {
+        "id": "kesar-mango",
+        "name": "Kesar (Saffron) Mango",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/kesar-mango.jpeg",
+                "description": "Kesar Mango, also known as Saffron Mango, is a variety of mango with a distinct saffron-like aroma and flavor. It is enjoyed for its sweet and rich taste and is often used to make desserts and beverages."
+            }
+        ]
+    },
+    {
+        "id": "pluot",
+        "name": "Pluot",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/pluot.jpeg",
+                "description": "Pluot is a hybrid fruit resulting from a cross between a plum and an apricot. It has a sweet and juicy flavor with hints of both parent fruits and is commonly enjoyed as a snack or used in cooking and baking."
+            }
+        ]
+    },
+    {
+        "id": "aprium",
+        "name": "Aprium",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/aprium.jpeg",
+                "description": "Aprium is a hybrid fruit that combines the flavors of apricot and plum. It has a sweet and tangy taste and is often eaten fresh or used in culinary creations."
+            }
+        ]
+    },
+    {
+        "id": "nectaplum",
+        "name": "Nectaplum",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/nectaplum.jpeg",
+                "description": "Nectaplum is a cross between a nectarine and a plum, offering a delightful blend of flavors. It has a sweet and juicy taste and can be enjoyed as a fresh snack or used in cooking."
+            }
+        ]
+    },
+    {
+        "id": "peacotum",
+        "name": "Peacotum",
+        "fruitItems": [
+            {
+                "image": "./assets/fruits/peacotum.jpeg",
+                "description": "Peacotum is a hybrid fruit that combines the flavors of peach, apricot, and plum. It has a juicy and aromatic flesh and is often enjoyed as a refreshing snack or used in culinary applications."
+            }
+        ]
+    }
+]
+
+
 const getFruitHtml = (fruits) => {
     fruits.map(fruit => {
         let html = `
@@ -150,21 +414,24 @@ const getFruitHtml = (fruits) => {
                 fruit.fruitItems.map(fItem => {
                 return `
             <p>${fItem.description}</p>
-            <img class="fruit-image image-medium" src="${fItem.image}">
-                    `;
+            <img class="fruit-image image-medium rounded" src="${fItem.image}">
+            <a class="d-block my-2" href="./form.html?fruitName=${fruit.name}">Rate it!!</a>           
+            `;
                 }).join("")
             }
         <div>
         `;
-        // fruit.fruitItems.map(fItem => {
-        //     return `
-        //         <p>${fItem.description}</p>
-        //         <img class="fruit-image image-medium" src="${fItem.image}>
-        //         `;
-        // }).join("");
         console.log(html);
         return html;
     });
 };
 
-getFruitHtml(fruits);
+const getAnchorLink = (fruits) => {
+    const anchorTags = fruits.map(fruit => {
+        return `<li><a href="./description.html#${fruit.id}">${fruit.name}</a></li>`;
+    }).join("\n");
+    console.log(anchorTags);
+};
+
+// getFruitHtml(fruits2);
+getAnchorLink(fruits2);
