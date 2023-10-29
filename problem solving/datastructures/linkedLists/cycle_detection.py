e5f295d672532c0d97b146e1d76f017242cfcef9
Revanth Madasu <revanthmadasu913@gmail.com>
2023-10-29 15:53:45 -0500

    improved performance - loops approach

--- problem solving/datastructures/linkedLists/cycle_detection.py
+++ problem solving/datastructures/linkedLists/cycle_detection.py
@@ -1,8 +1,7 @@
 '''
     problem: https://leetcode.com/problems/linked-list-cycle/
     concepts: recursion, linked lists
-    performance: 24.75% runtime, 5.85% memory
-    #todo - improve runtime, memory
+    performance: 96.32% Runtime, 38.44% memory
 '''
 from typing import Optional
 # Definition for singly-linked list.
@@ -11,6 +10,8 @@ class ListNode:
         self.val = x
         self.next = None
 
+# recursive approach
+# performance: 24.75% runtime, 5.85% memory
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if head == None:
@@ -23,3 +24,19 @@ class Solution:
                 return False
         else:
             return True
+
+# loops approach     
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if head == None:
+            return False
+        while head:
+            if not getattr(head, "visited", False):
+                if head.next != None:
+                    head.visited = True
+                    head = head.next
+                    continue
+                else:
+                    return False
+            else:
+                return True
