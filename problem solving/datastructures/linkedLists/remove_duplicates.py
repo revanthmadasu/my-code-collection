8ea18b8b8f4903761dbaefbf45f671932f86c557
Revanth Madasu <revanthmadasu913@gmail.com>
2023-11-01 21:37:34 -0500

    remove duplicates

---
+++ problem solving/datastructures/linkedLists/remove_duplicates.py
@@ -0,0 +1,28 @@
+from typing import Optional
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if (not head) or (not head.next):
+            return head
+        return self.chainPropogation(None, None, head)
+    def chainPropogation(self, acc_head, acc_last, nxt):
+        if not nxt:
+            return acc_head
+        cur_node = nxt
+        if cur_node.next and cur_node.next.val == cur_node.val:
+            while cur_node and nxt.val == cur_node.val:
+                cur_node = cur_node.next
+            return self.chainPropogation(acc_head, acc_last, cur_node)
+        else:
+            if acc_last:
+                new_node = ListNode(nxt.val)
+                acc_last.next = new_node
+                acc_last = new_node
+            else:
+                acc_head = ListNode(cur_node.val)
+                acc_last = acc_head
+            return self.chainPropogation(acc_head, acc_last, cur_node.next)
