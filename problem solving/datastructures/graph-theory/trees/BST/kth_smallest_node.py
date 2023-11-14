42e10253844a1a203485fc15cd448b15ec963ebf
Revanth Madasu <revanthmadasu913@gmail.com>
2023-11-14 10:21:56 -0600

    kth smallest node in bst - improved runtime

--- problem solving/datastructures/graph-theory/trees/BST/kth_smallest_node.py
+++ problem solving/datastructures/graph-theory/trees/BST/kth_smallest_node.py
@@ -1,7 +1,7 @@
 '''
     Problem: https://leetcode.com/problems/kth-smallest-element-in-a-bst
     concepts: BST, tree traversal, trees, recusion
-    performance: 39.22% runtime, 20.60% memory
+    performance: 80.06% runtime, 20.60% memory
     #todo: improve performance
 '''
 from typing import Optional
@@ -13,13 +13,22 @@ class TreeNode:
         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        inorder_traversal = self.getInorder(root)
+        self.k = k
+        inorder_traversal, l = self.getInorder(root)
         return inorder_traversal[k-1]
     def getInorder(self, node):
         left_traversal = []
         right_traversal = []
+        l = 1
+        ll = 0
+        rl = 0
         if node.left:
-            left_traversal = self.getInorder(node.left)
+            left_traversal, ll = self.getInorder(node.left)
+            # if traversal reaches upto k nodes then no need to visit right subtree
+            if ll + l > self.k:
+                traversal = [*left_traversal, node.val]
+                return (traversal, l+ll)
         if node.right:
-            right_traversal = self.getInorder(node.right)
-        return [*left_traversal, node.val, *right_traversal]
+            right_traversal, rl = self.getInorder(node.right)
+        traversal = [*left_traversal, node.val, *right_traversal]
+        return (traversal, l+ll+rl)
