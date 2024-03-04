03325346e6894c6e1d5d20941d67a1cdef66c7f6
Revanth Madasu <revanthmadasu913@gmail.com>
2024-03-03 18:54:09 -0600

    iterative approach

--- problem solving/algorithms/dynamic-programming/climbing-stairs.py
+++ problem solving/algorithms/dynamic-programming/climbing-stairs.py
@@ -22,6 +22,22 @@ class Solution(object):
             return self.result_dict[n]
         self.result_dict[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
         return self.result_dict[n]
+    
+        # iterative approach
+        # performance: 11.05% runtime, 92.86% memory
+        # def climbStairs(self, n):
+        #     """
+        #     :type n: int
+        #     :rtype: int
+        #     """
+        #     if n <= 3:
+        #         return n
+        #     n_ways = [0] * n
+        #     n_ways[0] = 1
+        #     n_ways[1] = 2
+        #     for i in range(2, n):
+        #         n_ways[i] = n_ways[i-1] + n_ways[i-2]
+        #     return n_ways[n-1]
 
 a = Solution()
 print(a.climbStairs(2))
