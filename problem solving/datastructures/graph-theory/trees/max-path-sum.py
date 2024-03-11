1222989c414db83d8794118d14baf8a72148ade9
Revanth Madasu <revanthmadasu913@gmail.com>
2024-03-11 10:23:26 -0500

    max path sum - opt

--- problem solving/datastructures/graph-theory/trees/max-path-sum.py
+++ problem solving/datastructures/graph-theory/trees/max-path-sum.py
@@ -1,7 +1,7 @@
 '''
     problem: https://leetcode.com/problems/binary-tree-maximum-path-sum
     concepts: tree
-    performance: 58.68% runtime, 31.42% memory
+    performance: 88.18% runtime, 55.91% memory
 '''
 from typing import Optional
 # Definition for a binary tree node.
@@ -14,21 +14,18 @@ class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self._max = float('-inf')
         def dfs(node):
-            _sum = 0
             leftSum = 0
             rightSum = 0
             if node.left:
                 leftSum = dfs(node.left)
-                _sum = leftSum
             if node.right:
                 rightSum = dfs(node.right)
-                if rightSum > _sum:
-                    _sum = rightSum
             max_sum = max(node.val, node.val+leftSum, node.val+rightSum, node.val+leftSum+rightSum)
-            max_path = max(_sum + node.val, node.val)
+            max_path = max(rightSum + node.val, leftSum + node.val, node.val)
             if self._max < max_sum:
                 self._max = max_sum
             return max_path
+
 
         dfs(root)
         return self._max
