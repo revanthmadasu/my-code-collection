346495365b819d8dbd28b954d7f15550b4911341
Revanth Madasu <revanthmadasu913@gmail.com>
2024-03-16 11:01:23 -0500

    alternate approach for group anagrams

--- problem solving/datastructures/hashtable/groupAnagrams.py
+++ problem solving/datastructures/hashtable/groupAnagrams.py
@@ -2,10 +2,10 @@
     problem: https://leetcode.com/problems/group-anagrams
     concepts: hashtable
     performance: 96.02% runtime, 95.35% memory
-    #todo: check if any better way of implementation exists n*str_len log(str_len) complexity
 '''
 from typing import List
 class Solution:
+    # time complexity n.k.log(k)
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         strsMap = dict()
         for string in strs:
@@ -15,3 +15,20 @@ class Solution:
             else:
                 strsMap[sortedString].append(string)
         return list(strsMap.values())
+    
+    # better time complexity n.k, but higher runtime in realtime
+    # def getSignature(self, s: str) -> str:
+    #     count = [0] * 26
+    #     for c in s:
+    #         count[ord(c) - ord('a')] += 1
+    #     return ''.join([f'{chr(i+ord("a"))}{count[i]}' for i in range(26) if count[i]])
+
+    # def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+    #     strsMap = dict()
+    #     for string in strs:
+    #         signature = self.getSignature(string)
+    #         if signature not in strsMap:
+    #             strsMap[signature] = [string]
+    #         else:
+    #             strsMap[signature].append(string)
+    #     return list(strsMap.values())
