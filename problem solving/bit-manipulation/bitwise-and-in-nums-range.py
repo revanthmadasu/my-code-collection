a711f41381a02812d9b48a6b0e161666dbc6c32b
Revanth Madasu <revanthmadasu913@gmail.com>
2024-03-19 21:15:56 -0500

    solution of range bitwise

--- problem solving/bit-manipulation/bitwise-and-in-nums-range.py
+++ problem solving/bit-manipulation/bitwise-and-in-nums-range.py
@@ -1,20 +1,21 @@
 '''
     problem: https://leetcode.com/problems/bitwise-and-of-numbers-range
     concepts: bit manipulation
-    #incomplete: 3 testcases failing
-    #todo - complete it
+    performance: 85.95% runtime, 66.34% memory
 '''
-import math
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        res = left
-        if left == 0 or right == 0:
-            return 0
-        if math.floor(math.log(left, 2)) != math.floor(math.log(right, 2)):
-            return 0
+        while right>left:
+            right &=(right-1)
+        return right
+        # res = left
+        # if left == 0 or right == 0:
+        #     return 0
+        # if math.floor(math.log(left, 2)) != math.floor(math.log(right, 2)):
+        #     return 0
 
-        for num in range(left+1, right+1):
-            res = res & num
-            if res == 0:
-                break
-        return res
+        # for num in range(left+1, right+1):
+        #     res = res & num
+        #     if res == 0:
+        #         break
+        # return res
