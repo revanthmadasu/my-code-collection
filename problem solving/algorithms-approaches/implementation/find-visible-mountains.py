face1dae8070b70e3a6eca62fc7cec0e1edaf123
Revanth Madasu <revanthmadasu913@gmail.com>
2024-03-24 15:05:51 -0500

    find visible mountains

--- problem solving/algorithms-approaches/implementation/find-visible-mountains.py
+++ problem solving/algorithms-approaches/implementation/find-visible-mountains.py
@@ -1,54 +1,71 @@
 '''
     problem: https://leetcode.com/problems/finding-the-number-of-visible-mountains
-    concepts: array, sorting
-    #incomplete: timeout error for 2 testcases
-    #todo: complete it
+    concepts: monotonic stack, array, sorting
+    performance: 29.38% runtime, 7.34% memory
 '''
 from typing import List
 class Solution:
     def visibleMountains(self, peaks: List[List[int]]) -> int:
-        def doesOverride(peak1, peak2):
-            if peak1[1] == peak2[1]:
-                return peak1[0] == peak2[0]
-            higherPeak, lowerPeak = (peak1, peak2) if peak1[1] > peak2[1] else (peak2, peak1)
-            # lower is on left side
-            if lowerPeak[0] <= higherPeak[0]:
-                return higherPeak[0] - lowerPeak[0] <= higherPeak[1] - lowerPeak[1]
-            else:
-                # lower on right side
-                return lowerPeak[0] - higherPeak[0]  <= higherPeak[1] - lowerPeak[1]
+        monotonic_stack = [float('inf')]
+        rightToLeft = set()
+        peaks.sort()
 
-        peaks.sort(key = lambda peak: peak[1])
-        curVisiblePeaks = []
-        visiblePeaks = 0
-        peaksVisited = dict()
-        redundantPeaks = set()
-        for peak in peaks:
-            if tuple(peak) in peaksVisited:
-                # print('redundant peak')
-                redundantPeaks.add(tuple(peak))
+        for i in range(len(peaks)-1, -1, -1):
+            peak = peaks[i]
+            x_inter = peak[0] - peak[1]
+            if tuple(peak) in rightToLeft:
+                print(f'removing {peak}')
+                rightToLeft.remove(tuple(peak))
                 continue
-            peaksVisited[tuple(peak)] = True
-            # print(f'previous visible peaks: {curVisiblePeaks}, curpeak: {peak}')
-            newVisiblePeaks = []
-            for visiblePeak in curVisiblePeaks:
-                # print(f'checking {visiblePeak}')
-                if doesOverride(visiblePeak, peak):
-                    # print(f'{peak} overrides {visiblePeak}')
-                    visiblePeaks -= 1
-                    # curVisiblePeaks.remove(visiblePeak)
-                else:
-                    # print(f'{peak} does not override {visiblePeak}')
-                    newVisiblePeaks.append(visiblePeak)
-            visiblePeaks += 1
-            curVisiblePeaks = newVisiblePeaks
-            curVisiblePeaks.append(peak)
+            if x_inter < monotonic_stack[len(monotonic_stack)-1]:
+                rightToLeft.add(tuple(peak))
+                monotonic_stack.append(x_inter)
+
+        monotonic_stack = [float('-inf')]
+        leftToRight = set()
+
+        for i in range(0, len(peaks)):
+            peak = peaks[i]
+            x_inter = peak[0] + peak[1]
+            if x_inter > monotonic_stack[len(monotonic_stack)-1]:
+                leftToRight.add(tuple(peak))
+                monotonic_stack.append(x_inter)
+        print(rightToLeft.intersection(leftToRight))
+        return len(rightToLeft.intersection(leftToRight))
+    
+        # Brute force approach
+        # def doesOverride(peak1, peak2):
+        #     if peak1[1] == peak2[1]:
+        #         return peak1[0] == peak2[0]
+        #     higherPeak, lowerPeak = (peak1, peak2) if peak1[1] > peak2[1] else (peak2, peak1)
+        #     # lower is on left side
+        #     if lowerPeak[0] <= higherPeak[0]:
+        #         return higherPeak[0] - lowerPeak[0] <= higherPeak[1] - lowerPeak[1]
+        #     else:
+        #         # lower on right side
+        #         return lowerPeak[0] - higherPeak[0]  <= higherPeak[1] - lowerPeak[1]
+
+        # peaks.sort(key = lambda peak: peak[1])
+        # curVisiblePeaks = []
+        # visiblePeaks = 0
+        # peaksVisited = dict()
+        # redundantPeaks = set()
         # for peak in peaks:
-        #     print(doesOverride([15, 39], peak))
-        #     print(doesOverride(peak, [15, 39]))
-        # print(doesOverride([15, 39], [38, 15]))
-        # print(doesOverride([38, 15], [15, 39]))
-        redundantVisiblePeaks = [redundantPeak for redundantPeak in redundantPeaks if list(redundantPeak) in curVisiblePeaks]
-        # print(f'final visible peaks: {curVisiblePeaks}')
-        # print(f'redundant visible peaks: {redundantVisiblePeaks}')
-        return len(curVisiblePeaks) - len(redundantVisiblePeaks)
+        #     if tuple(peak) in peaksVisited:
+        #         # print('redundant peak')
+        #         redundantPeaks.add(tuple(peak))
+        #         continue
+        #     peaksVisited[tuple(peak)] = True
+        #     # print(f'previous visible peaks: {curVisiblePeaks}, curpeak: {peak}')
+        #     newVisiblePeaks = []
+        #     for visiblePeak in curVisiblePeaks:
+        #         # print(f'checking {visiblePeak}')
+        #         if not doesOverride(visiblePeak, peak):
+        #             # print(f'{peak} does not override {visiblePeak}')
+        #             newVisiblePeaks.append(visiblePeak)
+        #     curVisiblePeaks = newVisiblePeaks
+        #     curVisiblePeaks.append(peak)
+
+        # redundantVisiblePeaks = [redundantPeak for redundantPeak in redundantPeaks if list(redundantPeak) in curVisiblePeaks]
+
+        # return len(curVisiblePeaks) - len(redundantVisiblePeaks)
