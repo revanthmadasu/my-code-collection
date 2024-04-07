e0d1dff5fc13e236e08ea8cfa53ec9c2db9d94f0
Revanth Madasu <revanthmadasu913@gmail.com>
2024-04-06 21:17:45 -0500

    improved performance - num of matching sequence

--- problem solving/datastructures/graph-theory/trie/num-matching-subseq.py
+++ problem solving/datastructures/graph-theory/trie/num-matching-subseq.py
@@ -2,35 +2,53 @@
     problem: https://leetcode.com/problems/number-of-matching-subsequences/
     concepts: Trie, Hashtable
     performance: 5.01% runtime, 5.02% memory
-    #todo: improve performance
 '''
 from typing import List
-class TrieNode:
-    def __init__(self, val, parent):
-        self.parent = parent
-        self.val = ''
-        self.nextCharNode = dict()
+# this straight forward approach is actually best performing - 90.52 runtime, 82.85 memory
 class Solution:
     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        trieRoot = TrieNode('', None)
-        prevNode = trieRoot
-        for ch in s:
-            node = TrieNode(ch, prevNode)
-            backTrackNode = prevNode
-            while backTrackNode and (ch not in backTrackNode.nextCharNode):
-                backTrackNode.nextCharNode[ch] = node
-                backTrackNode = backTrackNode.parent
-            prevNode = node
-        # resWords = []
-        subseqCount = 0
-        for word in words:
-            curNode = trieRoot
-            isSubseq = True
+    
+        def is_sub(word):
+            index=-1
             for ch in word:
-                if ch in curNode.nextCharNode:
-                    curNode = curNode.nextCharNode[ch]
-                else:
-                    isSubseq = False
-                    break
-            subseqCount += int(isSubseq)
-        return subseqCount
+                index=s.find(ch,index+1)
+                if index==-1:
+                    return False
+            return True
+        
+        c=0
+        for word in words:
+            if is_sub(word):
+                c+=1
+        
+        return c
+#performance: 5.01% runtime, 5.02% memory
+# class TrieNode:
+#     def __init__(self, val, parent):
+#         self.parent = parent
+#         self.val = ''
+#         self.nextCharNode = dict()
+# class Solution:
+#     def numMatchingSubseq(self, s: str, words: List[str]) -> int:
+#         trieRoot = TrieNode('', None)
+#         prevNode = trieRoot
+#         for ch in s:
+#             node = TrieNode(ch, prevNode)
+#             backTrackNode = prevNode
+#             while backTrackNode and (ch not in backTrackNode.nextCharNode):
+#                 backTrackNode.nextCharNode[ch] = node
+#                 backTrackNode = backTrackNode.parent
+#             prevNode = node
+#         # resWords = []
+#         subseqCount = 0
+#         for word in words:
+#             curNode = trieRoot
+#             isSubseq = True
+#             for ch in word:
+#                 if ch in curNode.nextCharNode:
+#                     curNode = curNode.nextCharNode[ch]
+#                 else:
+#                     isSubseq = False
+#                     break
+#             subseqCount += int(isSubseq)
+#         return subseqCount
