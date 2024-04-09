a864bb2db166b78a92d1d16728930a43e6c2d0e9
Revanth Madasu <revanthmadasu913@gmail.com>
2024-04-09 17:58:45 -0500

    find all recipes

---
+++ problem solving/algorithms-approaches/dfs/find-all-recipes.py
@@ -0,0 +1,34 @@
+'''
+    Problem: https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
+    Concepts: DFS, Recursion, Topological Sort
+    performance: 62.72% runtime, 5.89% memory
+'''
+from typing import List
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        n = len(recipes)
+        recipesIngredientsMap = dict()
+        for i in range(n):
+            recipesIngredientsMap[recipes[i]] = ingredients[i]
+        print(recipesIngredientsMap)
+        visiting = set()
+
+        @lru_cache(None)
+        def isPossible(foodMat):
+            # print(f'calling with {foodMat}')
+            if foodMat in visiting:
+                return False
+            possible = True
+            visiting.add(foodMat)
+            if foodMat in supplies:
+                possible = True
+            elif foodMat in recipesIngredientsMap:
+                for ingredient in recipesIngredientsMap[foodMat]:
+                    if not isPossible(ingredient):
+                        possible = False
+                        break
+            else:
+                possible = False
+            visiting.remove(foodMat)
+            return possible
+        return [recipe for recipe in recipes if isPossible(recipe)]
