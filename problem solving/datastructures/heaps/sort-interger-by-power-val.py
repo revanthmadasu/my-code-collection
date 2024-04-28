2de133f5bb18b4e5764d76fbb336e46ba54f09ca
Revanth Madasu <revanthmadasu913@gmail.com>
2024-04-28 09:21:04 -0500

    sort by pow val - sorting approach

--- problem solving/datastructures/heaps/sort-interger-by-power-val.py
+++ problem solving/datastructures/heaps/sort-interger-by-power-val.py
@@ -1,24 +1,33 @@
 '''
     Problem: https://leetcode.com/problems/sort-integers-by-the-power-value
-    Concepts: Heap, DP, LRU Cache
+    Concepts: Sorting, DP, LRU Cache
     performance: 63.67% runtime, 27.01% memory
 '''
-import heapq
 class Solution:
     def getKth(self, lo: int, hi: int, k: int) -> int:
         @lru_cache(maxsize=None)
         def getPowVal(num):
             if num == 1:
                 return 1
-            if num%2 == 0:
-                return 1 + getPowVal(num/2)
-            else:
-                return 1 + getPowVal(3*num + 1)
-        minHeap = []
-        heapq.heapify(minHeap)
-        for num in range(lo, hi+1):
-            heapq.heappush(minHeap, (getPowVal(num), num))
-        kthItem = None
-        for i in range(k):
-            kthItem = heapq.heappop(minHeap)
-        return kthItem[1]
+            return 1 + getPowVal(num/2 if num%2 == 0 else (3*num + 1))
+        return sorted(range(lo, hi+1), key=getPowVal)[k-1]
+# heap approach
+# import heapq
+# class Solution:
+#     def getKth(self, lo: int, hi: int, k: int) -> int:
+#         @lru_cache(maxsize=None)
+#         def getPowVal(num):
+#             if num == 1:
+#                 return 1
+#             if num%2 == 0:
+#                 return 1 + getPowVal(num/2)
+#             else:
+#                 return 1 + getPowVal(3*num + 1)
+#         minHeap = []
+#         heapq.heapify(minHeap)
+#         for num in range(lo, hi+1):
+#             heapq.heappush(minHeap, (getPowVal(num), num))
+#         kthItem = None
+#         for i in range(k):
+#             kthItem = heapq.heappop(minHeap)
+#         return kthItem[1]
