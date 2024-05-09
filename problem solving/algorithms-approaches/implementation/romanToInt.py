1c5655a757b6a47dead0023d98a579b8dfd6012f
Revanth Madasu <revanthmadasu913@gmail.com>
2024-05-09 12:26:14 -0500

    romanToInt oracle interview

--- problem solving/algorithms-approaches/implementation/romanToInt.py
+++ problem solving/algorithms-approaches/implementation/romanToInt.py
@@ -28,3 +28,17 @@ class Solution(object):
                 num += mapped[s[i]]
             i+=1
         return num
+    
+# asked in oracle interview - same solutions provided
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanStr = s
+        valuesMap = {'I': 1, 'V': 5, 'X':10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        res = valuesMap[romanStr[0]]
+        for i in range(1, len(romanStr)):
+            sym = romanStr[i]
+            prevSym = romanStr[i-1]
+            if valuesMap[sym] > valuesMap[prevSym]:
+                res -= (2 * valuesMap[prevSym])
+            res += valuesMap[sym]
+        return res
