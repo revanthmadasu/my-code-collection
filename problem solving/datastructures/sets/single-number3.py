a3ae749c56d3153d425e574a186f9ae8aeee0a6c
Revanth Madasu <revanthmadasu913@gmail.com>
2024-06-01 19:14:42 -0500

    single number 3

--- problem solving/datastructures/sets/single-number3.py
+++ problem solving/datastructures/sets/single-number3.py
@@ -4,9 +4,35 @@
     performance: 89.89% runtime, 41.53% memory
 '''
 from typing import List
+# class Solution:
+#     def singleNumber(self, nums: List[int]) -> List[int]:
+#         nums_set = set()
+#         for num in nums:
+#             nums_set.remove(num) if num in nums_set else nums_set.add(num)
+#         return list(nums_set)
+import math
+
 class Solution:
+    
     def singleNumber(self, nums: List[int]) -> List[int]:
-        nums_set = set()
+        def matches(num, bitPos):
+            binNum = bin(num)[2:]
+            return len(binNum) > bitPos and binNum[bitPos] == '1'
+        def getBitPos(num):
+            return int(math.log(num,2))
+        xor = 0
         for num in nums:
-            nums_set.remove(num) if num in nums_set else nums_set.add(num)
-        return list(nums_set)
+            xor = xor ^ num
+        print(f'xor is {xor}')
+        bitPos = getBitPos(xor)
+        doesMatch = 0
+        doesNotMatch = 0
+        for num in nums:
+            if matches(num, bitPos):
+                doesMatch = doesMatch ^ num
+            else:
+                doesNotMatch = doesNotMatch ^ num
+        return [doesMatch, doesNotMatch]
+    
+sol = Solution()
+print(sol.singleNumber([1,2,1,3,2,5]))
