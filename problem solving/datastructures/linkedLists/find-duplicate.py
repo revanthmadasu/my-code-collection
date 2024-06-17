7cd663b009bb2daff3f427c4478b1be2e9a56d65
Revanth Madasu <revanthmadasu913@gmail.com>
2024-06-17 18:55:42 -0500

    find duplicate

---
+++ problem solving/datastructures/linkedLists/find-duplicate.py
@@ -0,0 +1,19 @@
+'''
+    Problem: https://leetcode.com/problems/find-the-duplicate-number/
+    Concepts: Linked List
+    performance: 7.29% runtime, 24.71% memory
+'''
+from typing import List
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = nums[0]
+        fast = nums[nums[0]]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+        slow = 0
+        while slow != fast:
+            # print(f'slow: {slow} fast: {fast}')
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
