5b200d9c910458a3869164a4fcbe2dfc89d75c91
Revanth Madasu <revanthmadasu913@gmail.com>
2024-06-21 19:03:51 -0500

    improved perf

--- problem solving/algorithms-approaches/dfs/number-of-good-nodes.py
+++ problem solving/algorithms-approaches/dfs/number-of-good-nodes.py
@@ -1,7 +1,7 @@
 '''
     Problem: https://leetcode.com/problems/count-good-nodes-in-binary-tree/
     Concepts: DFS, Trees, Recursion
-    performance: 8.37% runtime, 94.55% memory
+    performance: 53.82% runtime, 39.09% memory
 '''
 # Definition for a binary tree node.
 class TreeNode:
@@ -12,16 +12,16 @@ class TreeNode:
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         self.goodCount = 0
-        def dfs(node, path):
+        def dfs(node):
+            prevMax = self.curMax
             if not node:
                 return
-            path.append(node.val)
-            if node.val >= max(path):
+            self.curMax = max(self.curMax, node.val)
+            if node.val >= self.curMax:
                 self.goodCount += 1
-            dfs(node.left, path)
-            dfs(node.right, path)
-            path.pop()
-        dfs(root, [])
-        return self.goodCount
-
-            
+            dfs(node.left)
+            dfs(node.right)
+            self.curMax = prevMax
+        self.curMax = root.val
+        dfs(root)
+        return self.goodCount  
