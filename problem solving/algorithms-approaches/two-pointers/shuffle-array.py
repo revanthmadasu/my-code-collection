4bd067a5718aa43bca84fe82df9d5aafa280df5c
Revanth Madasu <revanthmadasu913@gmail.com>
2024-06-29 14:17:40 -0500

    shuffle array

---
+++ problem solving/algorithms-approaches/two-pointers/shuffle-array.py
@@ -0,0 +1,13 @@
+'''
+    problem: https://leetcode.com/problems/shuffle-the-array/description/
+    Concepts: Array, Two Pointers
+    performance: 80.05% runtime, 47.82% memory
+'''
+from typing import List
+class Solution:
+    def shuffle(self, nums: List[int], n: int) -> List[int]:
+        res = []
+        for i in range(n):
+            res.append(nums[i])
+            res.append(nums[i+n])
+        return res
