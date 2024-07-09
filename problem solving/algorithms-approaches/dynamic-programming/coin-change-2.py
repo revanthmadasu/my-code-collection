ba8d9c943552ea493219147ee18623c4a0edfcf3
Revanth Madasu <revanthmadasu913@gmail.com>
2024-07-08 19:47:13 -0500

    coin change 2 - unbounded knapsack tabulation approach

--- problem solving/algorithms-approaches/dynamic-programming/coin-change-2.py
+++ problem solving/algorithms-approaches/dynamic-programming/coin-change-2.py
@@ -1,23 +1,39 @@
 '''
     problem: https://leetcode.com/problems/coin-change-ii/
-    concepts: Dynamic Programming, Unbounded Knapsack, DP Cache
-    performance: 9.07% runtime, 19.83% memory
-    #todo - implement dp using tabulation
+    concepts: Dynamic Programming, Unbounded Knapsack, DP Tabulation, DP Cache
+    performance: 39.30% runtime, 58.06% memory
 '''
 from typing import List
 from functools import cache
+#tabulation approach - better than caching
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def numWays(i, amount):
-            if i >= len(coins) and amount > 0:
-                return 0
-            if amount == 0:
-                return 1
-            tAmount = amount
-            curWays = numWays(i+1, tAmount)
-            while tAmount >= coins[i]:
-                tAmount -= coins[i]
-                curWays += numWays(i+1, tAmount)
-            return curWays
-        return numWays(0, amount)
+        dp = [[0] * (amount+1) for _ in range(len(coins)+1)]
+        for r in range(len(coins)):
+            dp[r][amount] = 1
+        for r in range(len(coins)-1, -1, -1):
+            for c in range(amount-1, -1,-1):
+                curAmount = amount - c
+                # amI = amount - i
+                amI = amount - curAmount + coins[r]
+                numWays = 0
+                # print(f'{r}, {c}, {amI}, {curAmount}')
+                dp[r][c] = dp[r+1][c] + (dp[r][amI] if curAmount - coins[r] >= 0 else 0)
+        return dp[0][0]
+
+# cache approach - performance: 9.07% runtime, 19.83% memory
+# class Solution:
+#     def change(self, amount: int, coins: List[int]) -> int:
+#         @cache
+#         def numWays(i, amount):
+#             if i >= len(coins) and amount > 0:
+#                 return 0
+#             if amount == 0:
+#                 return 1
+#             tAmount = amount
+#             curWays = numWays(i+1, tAmount)
+#             while tAmount >= coins[i]:
+#                 tAmount -= coins[i]
+#                 curWays += numWays(i+1, tAmount)
+#             return curWays
+#         return numWays(0, amount)
