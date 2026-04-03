633938d526b4a19eb5c4e02a70e00ddf2a6e3306
Revanth Madasu <revanthmadasu913@gmail.com>
2026-04-02 19:17:36 -0500

    revised - kth smallest node

--- problem solving/datastructures/graph-theory/trees/BST/kth_smallest_node.py
+++ problem solving/datastructures/graph-theory/trees/BST/kth_smallest_node.py
@@ -13,22 +13,14 @@ class TreeNode:
         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        self.k = k
-        inorder_traversal, l = self.getInorder(root)
-        return inorder_traversal[k-1]
-    def getInorder(self, node):
-        left_traversal = []
-        right_traversal = []
-        l = 1
-        ll = 0
-        rl = 0
-        if node.left:
-            left_traversal, ll = self.getInorder(node.left)
-            # if traversal reaches upto k nodes then no need to visit right subtree
-            if ll + l > self.k:
-                traversal = [*left_traversal, node.val]
-                return (traversal, l+ll)
-        if node.right:
-            right_traversal, rl = self.getInorder(node.right)
-        traversal = [*left_traversal, node.val, *right_traversal]
-        return (traversal, l+ll+rl)
+        def inorder(node, res):
+            if len(res) >= k:
+                return
+            if node.left:
+                inorder(node.left, res)
+            res.append(node.val)
+            if node.right:
+                inorder(node.right, res)
+        res = []
+        inorder(root, res)
+        return res[k-1]
