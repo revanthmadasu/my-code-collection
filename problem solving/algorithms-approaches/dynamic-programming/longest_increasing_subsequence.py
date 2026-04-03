e4150af2a79dacce50740939fba73652a05354f5
Revanth Madasu <revanthmadasu913@gmail.com>
2026-04-03 18:59:47 -0500

    revised - longest increasing subsequence

--- problem solving/algorithms-approaches/dynamic-programming/longest_increasing_subsequence.py
+++ problem solving/algorithms-approaches/dynamic-programming/longest_increasing_subsequence.py
@@ -7,20 +7,11 @@ from typing import List
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
-        lis = [1]*n
-        i = 0
-        while i < n:
-            # print(f'cur i: {i}')
-            prev_seq_indices = list(filter(lambda l_i: nums[l_i] < nums[i], range(0,i)))
-            # print(f'prev seqs: {prev_seq_indices}')
-            if len(prev_seq_indices):
-                prev_lis_vals = list(map(lambda l_i: lis[l_i], prev_seq_indices))
-                # print(f'prev lis vals: {prev_lis_vals}')
-                lis[i] += max(prev_lis_vals)
-                # print(f'changed lis to : {lis[i]}')
-
-            i += 1
-        return max(lis)
+        dp = [1] * n
+        for i in range(n):
+            seqs = [dp[x] for x in range(i) if nums[x] < nums[i]]
+            dp[i] = dp[i] + (max(seqs) if len(seqs) else 0)
+        return max(dp)
 
     # high performance - 70.48, 95.36
     # def lengthOfLIS(self, arr: List[int]) -> int:
