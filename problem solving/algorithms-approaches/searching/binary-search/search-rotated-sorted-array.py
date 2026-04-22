a636858ffd6b696abad079602acdcebe19c330ce
Revanth Madasu <revanthmadasu913@gmail.com>
2026-04-22 18:55:47 -0500

    revised - search in rotated sorted array

--- problem solving/algorithms-approaches/searching/binary-search/search-rotated-sorted-array.py
+++ problem solving/algorithms-approaches/searching/binary-search/search-rotated-sorted-array.py
@@ -1,47 +1,30 @@
 '''
     problem: https://leetcode.com/problems/search-in-rotated-sorted-array/
     concepts: binary search, searching, array
-    performance: 80.95% runtime, 89.48% memory
-    #todo: write clean code
+    performance: 100% runtime, 43.48% memory
 '''
 from typing import List
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start = 0
-        end = len(nums)-1
-        while start < end:
-            # print(f'searching between {start} - {end}')
-            mid = (start + end) // 2
-            isRotated = nums[start] > nums[end]
-            midRotated = nums[start] > nums[mid]
-            if target == nums[mid]:
+        left, right = 0, len(nums)-1
+        while right - left > 1:
+            # print(f'searching in {nums[left:right+1]}')
+            mid = (left+right) // 2
+            if nums[mid] == target:
                 return mid
-            elif target > nums[mid]:
-                if isRotated:
-                    if midRotated:
-                        if nums[end] >= target:
-                            start = mid+1
-                        else:
-                            end = mid-1
-                    else:
-                        start = mid+1
-                else:
-                    start = mid+1
-            elif target < nums[mid]:
-                if isRotated:
-                    if midRotated:
-                        end = mid-1
-                    else:
-                        if target >= nums[start]:
-                            end = mid-1
-                        else:
-                            start = mid+1
-                else:
-                    end = mid - 1
-        if target == nums[start]:
-            return start
-        elif target == nums[end]:
-            return end
-        else:
+            if (
+                (target > nums[right] and nums[mid] > nums[right] and target < nums[mid]) or 
+                (target > nums[right] and nums[mid] < nums[right]) or 
+                (target < nums[mid] and nums[mid] < nums[right])
+                ):
+            # if (target > nums[mid] and target > nums[right]) or (target < nums[mid] and (target > nums[left] or nums[mid] )):
+                right = mid-1
+            else:
+                left = mid + 1
+        # print(f'out of binary search - searching in {nums[left:right+1]}')
+        if target not in [nums[left], nums[right]]:
             return -1
+        if nums[left] == target:
+            return left
+        return right
                     
