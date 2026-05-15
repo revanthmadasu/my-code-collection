010ad47adfc9d4aa1ab97317909cf1d84554e567
Revanth Madasu <revanthmadasu913@gmail.com>
2026-05-15 18:45:25 -0500

    trapping water revised

--- problem solving/datastructures/arrays/trapping_rain_water.py
+++ problem solving/datastructures/arrays/trapping_rain_water.py
@@ -4,34 +4,18 @@
     concepts: arrays
     performance: 58.87% runtime, 76.34% memory
 '''
-class Solution(object):
-    def trap(self, height):
-        """
-        :type height: List[int]
-        :rtype: int
-        """
-        n = len(height)
-        left_prev_highest = []
-        cur_height_left = height[0]
-        max_height_left = cur_height_left
-
-        cur_height_right = height[n-1]
-        max_height_right = cur_height_right
-        right_next_highest = [0]*n
-        for i in range(n):
-            i_r = n-1-i
-            cur_height_left = height[i]
-            if cur_height_left > max_height_left:
-                max_height_left = cur_height_left
-            left_prev_highest.append(max_height_left)
-
-            cur_height_right = height[i_r]
-            if cur_height_right > max_height_right:
-                max_height_right = cur_height_right
-            right_next_highest[i_r] = max_height_right
-
-        water_stored = 0
-
-        for i in range(n):
-            water_stored += min(left_prev_highest[i], right_next_highest[i]) - height[i]
-        return water_stored
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        maxLeftHeights = []
+        maxRightHeights = []
+        _maxLeft, _maxRight = 0, 0
+        for i in range(len(height)):
+            _maxLeft = max(_maxLeft, height[i])
+            maxLeftHeights.append(_maxLeft)
+            _maxRight = max(_maxRight, height[len(height)-1-i])
+            maxRightHeights.append(_maxRight)
+        maxRightHeights = maxRightHeights[::-1]
+        storedWater = 0
+        for i in range(len(height)):
+            storedWater += max(0, min(maxRightHeights[i], maxLeftHeights[i])-height[i])
+        return storedWater
