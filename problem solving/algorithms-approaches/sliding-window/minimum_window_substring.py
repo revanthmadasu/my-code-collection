1807269878aada883b9f8e7a2f34319264a2bd35
Revanth Madasu <revanthmadasu913@gmail.com>
2026-05-17 19:28:49 -0500

    minimum window substring revised

--- problem solving/algorithms-approaches/sliding-window/minimum_window_substring.py
+++ problem solving/algorithms-approaches/sliding-window/minimum_window_substring.py
@@ -3,57 +3,39 @@
     concepts: Sliding Window, Two Pointers, Hash Table, String
     performance: 78.91% runtime, 66.14% memory
 '''
-import math
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need_map = {}
-        have_map = {}
-        for ch in t:
-            if ch not in need_map:
-                need_map[ch] = 0
-            need_map[ch] = need_map[ch] + 1
-            have_map[ch] = 0
-        satisfied_counts = 0
-        all_counts = len(need_map)
+        # sCharCountsMap = dict()
+        tCharCountsMap = dict()
+        charCountWindowMap = dict()
+        def countCharsIntoMap(string, map):
+            for char in string:
+                if char not in map:
+                    map[char] = 0
+                map[char] += 1
+        def windowContains():
+            for ch in tCharCountsMap:
+                if ch not in charCountWindowMap or charCountWindowMap[ch] < tCharCountsMap[ch]:
+                    return False
+            return True
+        # countCharsIntoMap(s, sCharCountsMap)
+        countCharsIntoMap(t, tCharCountsMap)
+        l,r = 0, 0
+        charCountWindowMap[s[l]] = 1
+        minWindowString = ""
 
-        n = len(s)
-        result = (-1, -1, math.inf)
-        left = 0
-        right = -1
-        while right < n:
-            # print(f'1 {have_map}, r: {right}, l: {left}, ss: {s[left:right+1]} res: {result}')
-            satisfied = satisfied_counts == all_counts
-            # if current window does not have string expand right - look for new character if it satisfies or not
-            if not satisfied:
-                # print("not satisfied")
-                right += 1
-                if right >= n:
-                    break
-                r_ch = s[right]
-                if r_ch in need_map:
-                    have_map[r_ch] += 1
-                    if need_map[r_ch] == have_map[r_ch]:
-                        satisfied_counts += 1
-                    if satisfied_counts == all_counts:
-                        # print(f"all counts satisfied,l: {left}, r: {right}, ss: {s[left: right+1]} ")
-                        if result[2] > right-left:
-                            result = (left, right, right-left)
+        while l <= r and r < len(s):
+            if windowContains():
+                # print(f'found possible between - l: {l}, r: {r}, string: {s[l:r+1]}')
+                if minWindowString == "" or len(minWindowString) > r-l+1:
+                    minWindowString = s[l:r+1]
+                charCountWindowMap[s[l]] -= 1
+                l += 1
             else:
-                # print('satisfied')
-                l_ch = s[left]
-                left += 1
-                if l_ch in need_map:
-                    have_map[l_ch] -= 1
-                    if need_map[l_ch] > have_map[l_ch]:
-                        satisfied_counts -= 1
-
-                if satisfied_counts == all_counts:
-                    # print(f"all counts satisfied,l: {left}, r: {right}, ss: {s[left: right+1]} ")
-                    if result[2] > right-left:
-                        result = (left, right, right-left)
-            # print(f'2 {have_map}, r: {right}, l: {left}, ss: {s[left:right+1]} res: {result}')
-        if result[2] == math.inf:
-            return ""
-        else:
-            return s[result[0]:result[1]+1]
+                r += 1
+                if r < len(s):
+                    if s[r] not in charCountWindowMap:
+                        charCountWindowMap[s[r]] = 0
+                    charCountWindowMap[s[r]] += 1
+        return minWindowString
         
